3776720d7d49344e9de138a92abd474954c0187f
Allie Neal <allieneal93@gmail.com>
2021-07-08 15:06:36 -0400

    adds views and routes for bridal_attire

--- app/controllers/api/brideattire_controller.rb
+++ app/controllers/api/brideattire_controller.rb
@@ -1,2 +1,12 @@
 class Api::BrideattireController < ApplicationController
+
+  def index
+    @bride_attire = BrideAttire.all 
+    render "index.json.jb"
+  end
+
+  def show
+    @bride_attire = BrideAttire.find_by(id: params[:id])
+    render "show.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/brideattire" => "bride_attire#index"
+    get "/brideattire/:id" => "bride_attire#show"
+    post "/brideattire" => "bride_attire#create"
+    patch "/brideattire/:id" => "bride_attire#edit"
+  end
 end
